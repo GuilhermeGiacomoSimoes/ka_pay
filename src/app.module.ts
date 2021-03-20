ed612ac8869313e05221fb3c90e8399c77b1ee5e
Murilo Bezerril <murilo.bezerril@gmail.com>
2021-03-20 16:08:20 -0300

    feat: correçao rebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { V1Module } from './v1/v1.module';
 
 @Module({
   imports: [V1Module],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
