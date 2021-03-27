106ae389ad17b6a3ed4f8e208e368921a1664d00
Murilo Bezerril <murilo.bezerril@gmail.com>
2021-03-27 13:09:45 -0300

    feat: criando a interface de uma loja

---
+++ src/v1/shop/dto/create-shop.dto.ts
@@ -0,0 +1,5 @@
+export class CreateShopDto {
+  name: string;
+  description: string;
+  product: string;
+}
---
+++ src/v1/shop/interface/shop.interface.ts
@@ -0,0 +1,8 @@
+export interface IShop {
+  name: string;
+  description: string;
+  product: string;
+  pv: string;
+  token: string;
+  created_in: Date;
+}
