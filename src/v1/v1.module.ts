36b43d14f493c81329fd6d69380e604b46678e1a
Murilo Bezerril <murilo.bezerril@gmail.com>
2021-03-27 13:22:16 -0300

    feat: adicionando modulo de loja

--- src/v1/v1.module.ts
+++ src/v1/v1.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { V1Controller } from './v1.controller';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
-  controllers: [V1Controller]
+  controllers: [V1Controller],
+  imports: [ShopModule],
 })
 export class V1Module {}
