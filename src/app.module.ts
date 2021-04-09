6f26902c4e143518b663afcb4c3af1f8ae394fcd
Guilherme <trintaeoitogc@gmail.com>
2021-04-08 22:41:29 -0300

    feat: adicionado a conexão do banco ao app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
 import { TerminusModule } from '@nestjs/terminus';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [V1Module, TerminusModule],
+	imports: [V1Module, TerminusModule, TypeOrmModule.forRoot({
+		type: 'mysql',
+		host: 'localhost',
+		port: 3306,
+		username: 'root',
+		password: 'root',
+		database: 'kpay',
+		entities: [],
+		synchronize: true,
+	})],
   controllers: [],
   providers: [],
 })
