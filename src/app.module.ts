8b2393cb907a704f73defe939c96abcf3f2651ab
Guilherme <trintaeoitogc@gmail.com>
2021-04-08 22:36:27 -0300

    feat: adicionado o terminusModule no app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [V1Module],
+  imports: [V1Module, TerminusModule],
   controllers: [],
   providers: [],
 })
