b1732b083963b925129557858a0737cc801dbeb1
Guilherme <trintaeoitogc@gmail.com>
2021-04-10 10:46:21 -0300

    fix: resolve conflits

--- src/v1/v1.module.ts
+++ src/v1/v1.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { V1Controller } from './v1.controller';
-<<<<<<< HEAD
 import { V1HealthController } from './health/v1.health.controller';
 import { TerminusModule } from '@nestjs/terminus';
-
-@Module({
-  imports: [TerminusModule],
-  controllers: [V1Controller, V1HealthController],
-=======
 import { ShopkeeperModule } from './shopkeeper/shopkeeper.module';
 
 @Module({
-  controllers: [V1Controller],
-  imports: [ShopkeeperModule]
->>>>>>> 60b5c0ad03c45734e695dbffad4782f8b84e42d7
+  imports: [TerminusModule, ShopkeeperModule],
+  controllers: [V1Controller, V1HealthController, V1Controller],
 })
 export class V1Module {}
