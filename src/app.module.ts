9a0846a5fb7e7d30dd123e200ed62c3ec7b02795
Luís Mascarenhas <luis.mascarenhas@kabum.com.br>
2021-04-17 10:54:25 -0300

    feat: modularizado database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configMySQLDocker } from 'ormconfig';
 
 @Module({
-	imports: [V1Module, TerminusModule, TypeOrmModule.forRoot({
-		type: 'mysql',
-		host: 'localhost',
-		port: 3306,
-		username: 'root',
-		password: 'root',
-		database: 'kpay',
-		entities: [],
-		synchronize: true,
-	})],
+  imports: [V1Module, TerminusModule, TypeOrmModule.forRoot(configMySQLDocker)],
   controllers: [],
   providers: [],
 })
