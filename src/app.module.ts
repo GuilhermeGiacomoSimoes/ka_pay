30deb8289b614dba55bb1e3ad94669472d3d5e1f
Guilherme <trintaeoitogc@gmail.com>
2021-04-24 08:21:12 -0300

    fix: colocado a conexão com o localhosto ao invés do docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configMySQLDocker } from 'ormconfig';
+import { configMySqlLocal } from 'ormconfig';
 
 @Module({
-  imports: [V1Module, TerminusModule, TypeOrmModule.forRoot(configMySQLDocker)],
+  imports: [V1Module, TerminusModule, TypeOrmModule.forRoot(configMySqlLocal)],
   controllers: [],
   providers: [],
 })
