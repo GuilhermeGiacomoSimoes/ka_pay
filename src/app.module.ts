8a412db80e6654d4eba972d5732e6d648a59a897
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-05-29 13:47:57 -0300

    feat: importado os novos files no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { V1Module } from './v1/v1.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configMySQLDocker } from 'ormconfig';
+import { EstablishmentController } from './v1/establishment/establishment.controller';
+import { EstablishmentService } from './v1/establishment/establishment.service';
+import { EstablishmentModule } from './v1/establishment/establishment.module';
 
 @Module({
-  imports: [V1Module, TerminusModule, TypeOrmModule.forRoot(configMySQLDocker)],
-  controllers: [],
-  providers: [],
+  imports: [
+    V1Module,
+    TerminusModule,
+    TypeOrmModule.forRoot(configMySQLDocker),
+    EstablishmentModule,
+  ],
+  controllers: [EstablishmentController],
+  providers: [EstablishmentService],
 })
 export class AppModule {}
