7b9aae9c2d5845658b6272a15da54092281fcebf
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-05-30 09:48:14 -0300

    feat: formatado a entitade pro typeorm

--- src/v1/establishment/entities/establishment.entity.ts
+++ src/v1/establishment/entities/establishment.entity.ts
@@ -1,9 +1,25 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
 export class Establishment {
+  @PrimaryColumn()
   uuid: string;
+
+  @Column({ length: 100 })
   name: string;
+
+  @Column({ length: 100 })
   street: string;
+
+  @Column('int')
   number: number;
+
+  @Column({ length: 14 })
   cnpj: string;
+
+  @Column({ length: 100 })
   city: string;
+
+  @Column('int')
   IE: number;
 }
