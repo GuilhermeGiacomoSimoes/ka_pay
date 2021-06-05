d23c4ecf119dee059f98053032c2233451dc26f3
Marcos Medeiros <Marcos Csar Medeiros>
2021-06-05 14:56:54 -0300

    feat: routes e services do shopkeeper

--- src/v1/shopkeeper/shopkeeper.controller.ts
+++ src/v1/shopkeeper/shopkeeper.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Res, HttpStatus, Body, Delete, Param } from '@nestjs/common';
+import { Response } from "express";
+import { CreateShopkeeperDTO } from './dto/create.shopkeeper.dto';
 
 @Controller('shopkeeper')
-export class ShopkeeperController {}
+export class ShopkeeperController {
+    @Post()
+    create(@Res() res: Response, @Body() createShopkeeper: CreateShopkeeperDTO) {
+        res.status(HttpStatus.CREATED).send({ error: 0, message: 'Has been created'});
+    }
+
+    @Get()
+    findAll(@Res() res: Response): any {
+        res.status(HttpStatus.OK).json([
+            {
+                uuid: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+                name: 'Marcos Medeiros',
+                email: 'marcos.medeiros@kabum.com.br',
+                document: '123.456.789-00',
+                token: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+                phone: '5519912345678',
+                account_type: 'JURIDIC',
+            }
+        ])
+    }
+    
+    @Get(':id')
+    findOne(@Param() uuid: string, @Res() res: Response): any {
+        res.status(HttpStatus.OK).json({
+                uuid: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+                name: 'Marcos Medeiros',
+                email: 'marcos.medeiros@kabum.com.br',
+                document: '123.456.789-00',
+                token: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+                phone: '5519912345678',
+                account_type: 'JURIDIC',
+        })
+    }
+
+    @Put(':id')
+    update(@Param() uuid: string, @Res() res: Response, @Body() createShopkeeper: CreateShopkeeperDTO) {
+        res.status(HttpStatus.PARTIAL_CONTENT).send({ error: 0, message: 'Has been updated'});
+    }
+
+    @Delete(':id')
+    remove(@Param() uuid: string, @Res() res: Response) {
+        res.status(HttpStatus.NO_CONTENT).send();
+    }
+}
