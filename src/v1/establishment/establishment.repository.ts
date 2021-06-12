1ab6965f446ec7d7c5edc34fafae8a9f6e398dd6
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-06-12 11:02:23 -0300

    feat: decodado o repository como injectable

--- src/v1/establishment/establishment.repository.ts
+++ src/v1/establishment/establishment.repository.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { Establishment } from './entities/establishment.entity';
 
+@Injectable()
 @EntityRepository(Establishment)
 export class EstablishmentRepository extends Repository<Establishment> {
   async getEstablishment(id: number): Promise<Establishment> {
