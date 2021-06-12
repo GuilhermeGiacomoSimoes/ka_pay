94248740a214b50a3fffc0928e3a7353552407ff
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-06-12 11:01:29 -0300

    feat: colocado os imports, providers e controller no establishment.module

--- src/v1/establishment/establishment.module.ts
+++ src/v1/establishment/establishment.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Establishment } from './entities/establishment.entity';
+import { EstablishmentController } from './establishment.controller';
+import { EstablishmentRepository } from './establishment.repository';
+import { EstablishmentService } from './establishment.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Establishment])],
+  providers: [EstablishmentService, EstablishmentRepository, Establishment],
+  controllers: [EstablishmentController],
+})
 export class EstablishmentModule {}
