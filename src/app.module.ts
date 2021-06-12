5bfc1c3ba71acaf2fbb271bce761e299b14aee11
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-06-12 10:59:52 -0300

    feat: tirado as instanciar de controller e service do app.module pq não fazem o menor sentido

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { V1Module } from './v1/v1.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configMySQLDocker } from 'ormconfig';
-import { EstablishmentController } from './v1/establishment/establishment.controller';
-import { EstablishmentService } from './v1/establishment/establishment.service';
+import { configMySQLDocker, configMySqlLocal } from 'ormconfig';
 import { EstablishmentModule } from './v1/establishment/establishment.module';
 
 @Module({
@@ -14,7 +12,7 @@ import { EstablishmentModule } from './v1/establishment/establishment.module';
     TypeOrmModule.forRoot(configMySQLDocker),
     EstablishmentModule,
   ],
-  controllers: [EstablishmentController],
-  providers: [EstablishmentService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
