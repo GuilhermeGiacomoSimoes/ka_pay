1361d6bdcc4517ed252e04a8ff5a0753b79cbb1f
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-07-17 08:52:34 -0300

    chore: creating functions from repository

--- src/v1/establishment/establishment.repository.ts
+++ src/v1/establishment/establishment.repository.ts
@@ -1,7 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateEstablishmentDTO } from './dto/create.establishment.dto';
+import { UpdateEstablish } from './dto/update.establishment.dto';
 import { Establishment } from './entities/establishment.entity';
 
 @Injectable()
 @EntityRepository(Establishment)
-export class EstablishmentRepository extends Repository<Establishment> {}
+export class EstablishmentRepository {
+  constructor(
+    @InjectRepository(Establishment)
+    private repositoryORM: Repository<Establishment>,
+  ) {}
+
+  public async findAll(): Promise<Establishment[]> {
+    return await this.repositoryORM.find({});
+  }
+
+  public async findById(uuid: string): Promise<Establishment> {
+    return await this.repositoryORM.findOne(uuid);
+  }
+
+  public async createEstablishment(
+    establishment: CreateEstablishmentDTO,
+  ): Promise<Establishment> {
+    return await this.repositoryORM.save(establishment);
+  }
+
+  public async updateEstablishment(establishment: UpdateEstablish) {
+    return await this.repositoryORM.save(establishment);
+  }
+
+  public async removeEstablishment(uuid: string) {
+    const establishment = await this.repositoryORM.findOne(uuid);
+    return await this.repositoryORM.remove(establishment);
+  }
+}
