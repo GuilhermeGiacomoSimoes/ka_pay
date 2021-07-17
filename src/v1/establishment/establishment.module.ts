d44bfeca86a6fbbc17b2d14308d4002e83ea5ddd
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-07-17 08:51:59 -0300

    style: changes made by eslint

--- src/v1/establishment/establishment.module.ts
+++ src/v1/establishment/establishment.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Establishment } from './entities/establishment.entity';
 import { EstablishmentController } from './establishment.controller';
-import { EstablishmentRepository } from './establishment.repository';
 import { EstablishmentService } from './establishment.service';
+import { establishmentRepositoryProvider } from './establishment.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Establishment])],
-  providers: [EstablishmentService, EstablishmentRepository, Establishment],
+  providers: [
+    EstablishmentService,
+    establishmentRepositoryProvider,
+    Establishment,
+  ],
   controllers: [EstablishmentController],
 })
 export class EstablishmentModule {}
