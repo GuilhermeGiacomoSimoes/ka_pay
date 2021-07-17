a35af08eedaaaa1ee8f109fd808d3037b143eb3f
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-07-17 12:19:32 -0300

    test: init  create a tests

--- src/v1/establishment/establishment.service.spec.ts
+++ src/v1/establishment/establishment.service.spec.ts
@@ -1,8 +1,14 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateEstablishmentDTO } from './dto/create.establishment.dto';
+import { EstablishmentRepository } from './establishment.repository';
 import { EstablishmentService } from './establishment.service';
 
 describe('EstablishmentService', () => {
   let service: EstablishmentService;
+  let repository: EstablishmentRepository;
+  let mockData: Object;
+  let mockDataError: Object;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +16,38 @@ describe('EstablishmentService', () => {
     }).compile();
 
     service = module.get<EstablishmentService>(EstablishmentService);
+    repository = module.get<EstablishmentRepository>(EstablishmentRepository);
+
+    mockData = {
+      uuid: expect.any(String),
+      name: 'KaBuM!',
+      street: 'Rua Carlos Gomes',
+      number: 123,
+      cnpj: '123/100',
+      city: 'Limeira',
+      IE: 123,
+    };
+
+    mockDataError = {
+      marcao: 'teste',
+    };
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createEstablishment', () => {
+    it('Check save establishment in bd', async () => {
+      await expect(
+        repository.createEstablishment(mockData as CreateEstablishmentDTO),
+      ).resolves.not.toThrow();
+    });
+
+    it('Return error if parse incorrect payload', async () => {
+      await expect(
+        repository.createEstablishment(mockDataError as CreateEstablishmentDTO),
+      ).resolves.toThrow();
+    });
   });
 });
