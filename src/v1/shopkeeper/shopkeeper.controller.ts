8f4455b8401f05a841aed935ffbaca599bab5a03
Luís Mascarenhas <luis.mascarenhas@kabum.com.br>
2021-07-25 17:37:54 -0300

    feat: criado rota para retornar um lojista

--- src/v1/shopkeeper/shopkeeper.controller.ts
+++ src/v1/shopkeeper/shopkeeper.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ShopKeeper } from './entities/shopkeeper.entity';
+import { ShopkeeperService } from './shopkeeper.service';
 
 @Controller('shopkeeper')
-export class ShopkeeperController {}
+export class ShopkeeperController {
+  constructor(private readonly shopKeeperService: ShopkeeperService) {}
+
+  @Get('/:id')
+  async getShopKeeper(@Param('id') id: number): Promise<ShopKeeper> {
+    return await this.shopKeeperService.getShopKeeper(id);
+  }
+}
