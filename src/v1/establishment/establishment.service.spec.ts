75d40eaf9381e2cc21b0dd09325c982dbd826594
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-08-14 18:11:39 -0300

    test: criado os testes da service

--- src/v1/establishment/establishment.service.spec.ts
+++ src/v1/establishment/establishment.service.spec.ts
@@ -1,18 +1,32 @@
 import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateEstablishmentDTO } from './dto/create.establishment.dto';
+import { UpdateEstablish } from './dto/update.establishment.dto';
 import { EstablishmentRepository } from './establishment.repository';
 import { EstablishmentService } from './establishment.service';
 
 describe('EstablishmentService', () => {
   let service: EstablishmentService;
   let repository: EstablishmentRepository;
-  let mockData: Object;
-  let mockDataError: Object;
+  let mockData;
+  let mockDataError;
 
   beforeEach(async () => {
+    const EstablishmentRepositoryStub = {
+      findById: jest.fn(),
+      createEstablishment: jest.fn(),
+      updateEstablishment: jest.fn(),
+      removeEstablishment: jest.fn(),
+      findAll: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EstablishmentService],
+      providers: [
+        EstablishmentService,
+        {
+          provide: EstablishmentRepository,
+          useFactory: () => EstablishmentRepositoryStub,
+        },
+      ],
     }).compile();
 
     service = module.get<EstablishmentService>(EstablishmentService);
@@ -26,11 +40,11 @@ describe('EstablishmentService', () => {
       cnpj: '123/100',
       city: 'Limeira',
       IE: 123,
-    };
+    } as Object;
 
     mockDataError = {
       marcao: 'teste',
-    };
+    } as Object;
   });
 
   it('should be defined', () => {
@@ -40,14 +54,82 @@ describe('EstablishmentService', () => {
   describe('createEstablishment', () => {
     it('Check save establishment in bd', async () => {
       await expect(
-        repository.createEstablishment(mockData as CreateEstablishmentDTO),
+        service.createEstablishment(mockData),
       ).resolves.not.toThrow();
     });
 
     it('Return error if parse incorrect payload', async () => {
+      (repository.createEstablishment as jest.Mock).mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+      await expect(service.createEstablishment(mockDataError)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+  });
+
+  describe('updateEstablishment', () => {
+    it('Check update establishment in bd', async () => {
       await expect(
-        repository.createEstablishment(mockDataError as CreateEstablishmentDTO),
-      ).resolves.toThrow();
+        service.updateEstablishment(mockData as UpdateEstablish),
+      ).resolves.not.toThrow();
+    });
+
+    it('Return error if parse incorrect payload', async () => {
+      (repository.updateEstablishment as jest.Mock).mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+      await expect(
+        service.updateEstablishment(mockDataError as UpdateEstablish),
+      ).rejects.toThrow(new InternalServerErrorException());
+    });
+  });
+
+  describe('getEstablishment', () => {
+    it('check get one establishment', async () => {
+      (repository.findById as jest.Mock).mockReturnValueOnce(mockData);
+      expect(await service.getEstablishment('1')).toEqual(mockData);
+    });
+    it('check error from get one establishment', async () => {
+      (repository.findById as jest.Mock).mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+
+      expect(service.getEstablishment('4')).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+  });
+
+  describe('remove Establishment', () => {
+    it('Check remove establishment in bd', async () => {
+      await expect(
+        service.removeEstablishment('1f6e81eb-ef03-4002-92e1-e3d2e70e8647'),
+      ).resolves.not.toThrow();
+    });
+
+    it('Return error if parse incorrect id', async () => {
+      (repository.removeEstablishment as jest.Mock).mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+      await expect(service.removeEstablishment(null)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+  });
+
+  describe('get all Establishments', () => {
+    it('Check get all establishments', async () => {
+      await expect(service.getAllEstablishment()).resolves.not.toThrow();
+    });
+
+    it('Return error if parse incorrect id', async () => {
+      (repository.findAll as jest.Mock).mockRejectedValueOnce(
+        new InternalServerErrorException(),
+      );
+      await expect(service.getAllEstablishment()).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
     });
   });
 });
