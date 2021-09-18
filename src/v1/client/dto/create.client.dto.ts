ea8dee182a2b1db8aed386df72bd5fc96dacd67e
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-09-18 14:57:25 -0300

    feat: criado o diretorio de cliente, e criado o create dto e a entidade

---
+++ src/v1/client/dto/create.client.dto.ts
@@ -0,0 +1,12 @@
+export class Client {
+  uuid: string;
+  name: string;
+  birthDate: number;
+  cpfCnpj: number;
+  addressStreet: string;
+  addressNumber: number;
+  addressNeighborhood: string;
+  addressCity: string;
+  fraudster: boolean;
+  addressState: string;
+}
---
+++ src/v1/client/entities/client.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('client')
+export class Client {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column('int')
+  birthDate: number;
+
+  @Column('int')
+  cpfCnpj: number;
+
+  @Column({ length: 70 })
+  addressStreet: string;
+
+  @Column('int')
+  addressNumber: number;
+
+  @Column({ length: 50 })
+  addressNeighborhood: string;
+
+  @Column({ length: 50 })
+  addressCity: string;
+
+  @Column('bit')
+  fraudster: boolean;
+
+  @Column({ length: 2 })
+  addressState: string;
+}
