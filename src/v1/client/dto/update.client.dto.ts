cd920035a36b969f1fa29e4d7772ee5a95885a4f
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-10-16 17:43:56 -0300

    feat: criado o dto de update do client

---
+++ src/v1/client/dto/update.client.dto.ts
@@ -0,0 +1,12 @@
+export class UpdateClientDTO {
+  uuid: string;
+  name: string;
+  birthDate: number;
+  cpfCnpj: number;
+  addressStreet: string;
+  addressNumber: number;
+  addressNeighborhood: string;
+  addressCity: string;
+  fraudster: boolean;
+  addressState: string;
+}
