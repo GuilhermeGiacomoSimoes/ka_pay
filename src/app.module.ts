0f3d3d54c620064a998df7ef8ef6fac4ad4c6859
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-10-16 17:41:46 -0300

    chore: adicionado o controller e o service de client no module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configMySQLDocker } from 'ormconfig';
 import { EstablishmentModule } from './v1/establishment/establishment.module';
+import { ClientController } from './client/client.controller';
+import { ClientService } from './v1/client/client.service';
 
 @Module({
   imports: [
@@ -12,7 +14,7 @@ import { EstablishmentModule } from './v1/establishment/establishment.module';
     TypeOrmModule.forRoot(configMySQLDocker),
     EstablishmentModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ClientController],
+  providers: [ClientService],
 })
 export class AppModule {}
