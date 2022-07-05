cb1daa85ad8d92859d0edf9b27cf2aa42d93dc9c
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-04 21:19:07 -0300

    add use-case in module

--- src/v1/application/dependency-inversion/v1.module.ts
+++ src/v1/application/dependency-inversion/v1.module.ts
@@ -1,8 +1,9 @@
 import {Module} from "@nestjs/common";
+import {ListTransactionUseCase} from "src/v1/business/domain/use-case/list-transaction.use-case";
 import TransactionController from "../api/controller/transaction.controller";
 
 @Module({
 	controllers: [TransactionController], 
-	providers: []
+	providers: [ListTransactionUseCase]
 })
 export class v1Module {}
