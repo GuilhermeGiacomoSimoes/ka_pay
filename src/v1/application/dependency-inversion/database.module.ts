6c4e684c53429a09afe2d131ef9fdc59774b9bf7
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-12 22:29:13 -0300

    feat: create a module from database

---
+++ src/v1/application/dependency-inversion/database.module.ts
@@ -0,0 +1,9 @@
+import {Module} from "@nestjs/common";
+import {databaseProviders} from "src/v1/infrastructure/config/database.config";
+
+
+@Module({
+	providers: [... databaseProviders],
+	exports: [... databaseProviders]
+})
+export class DatabaseModule {}
