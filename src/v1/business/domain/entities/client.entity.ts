be55c3915c144d0092de2e0fca57bb5c62484f09
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-12 23:24:46 -0300

    feat: create entitys

---
+++ src/v1/business/domain/entities/client.entity.ts
@@ -0,0 +1,9 @@
+
+export class Client {
+	private _id : string;
+	private _name : string;
+	private _email : string;
+	private _birthDate : string;
+	private _cpfCnpj : string;
+	private _money : number;
+}
