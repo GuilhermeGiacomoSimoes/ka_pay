73213725e5303cb8237eed72c565c80e4de593fb
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-12 23:23:41 -0300

    feat: controller implementation

--- src/v1/application/api/controller/transaction.controller.ts
+++ src/v1/application/api/controller/transaction.controller.ts
@@ -1,9 +1,16 @@
 import {Controller, Get} from "@nestjs/common";
+import {Transaction} from "src/v1/business/domain/entities/transaction.entity";
+import {ListTransactionUseCase} from "src/v1/business/domain/use-case/list-transaction.use-case";
 
 @Controller('transaction')
 export default class TransactionController {
 
+	constructor(
+		private readonly listTransationsUseCases : ListTransactionUseCase,	
+	) {}
+
 	@Get()
-	async listTransations() : Promise<void> | never {
+	async listTransations() : Promise<Transaction> | never {
+		return this.listTransationsUseCases.execute();
 	}
 }
