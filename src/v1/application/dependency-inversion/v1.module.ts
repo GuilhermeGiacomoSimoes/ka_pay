5602d7ce0d992f35425e41c923723f19ffb68aed
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-12 23:24:12 -0300

    feat: create a function list transactions

--- src/v1/application/dependency-inversion/v1.module.ts
+++ src/v1/application/dependency-inversion/v1.module.ts
@@ -1,9 +1,11 @@
 import {Module} from "@nestjs/common";
 import {ListTransactionUseCase} from "src/v1/business/domain/use-case/list-transaction.use-case";
 import TransactionController from "../api/controller/transaction.controller";
+import {DatabaseModule} from "./database.module";
 
 @Module({
 	controllers: [TransactionController], 
-	providers: [ListTransactionUseCase]
+	providers: [ListTransactionUseCase], 
+	imports : [DatabaseModule]
 })
 export class v1Module {}
