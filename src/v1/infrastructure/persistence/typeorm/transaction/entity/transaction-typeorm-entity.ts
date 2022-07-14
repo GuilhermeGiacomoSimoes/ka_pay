e110b1c982720b00a9f476932ab20c113a2f81f7
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-13 23:23:25 -0300

    feat: typeorm entity create

--- src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity.ts
@@ -1,4 +1,16 @@
-import {Entity} from "typeorm";
+import {Column, Entity, PrimaryColumn} from "typeorm";
 
 @Entity('transaction')
-export class TransactionTypeORMEntity {}
+export class TransactionTypeORMEntity {
+	@PrimaryColumn()
+	id : string;
+
+	@Column({ name : 'value_transaction' })
+	valueTransactio : number;
+
+	@Column({ name : 'client_destination' })
+	clientDestination : string;
+
+	@Column({ name : 'client_origin' })
+	clientOrigin : string;
+}
