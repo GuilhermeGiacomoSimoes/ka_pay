b796b3fd72a5f4bdb9864a1b104d6bc7c0f146ee
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-13 23:23:09 -0300

    temp: is not necessarry now

--- src/v1/business/domain/use-case/list-transaction.use-case.ts
+++ src/v1/business/domain/use-case/list-transaction.use-case.ts
@@ -1,32 +1,10 @@
-import {Inject} from "@nestjs/common";
 import {TransactionResponse} from "../../common/Transaction.response";
 
 export class ListTransactionUseCase {
 
 	constructor(
-		@Inject()
 	) {}
 
 	execute() : Promise<TransactionResponse> {
-		return [
-			{
-				id : "",
-				valueTransactio : 100,
-				clientDestination : {
-					name : ,
-					email : "",
-					birthDate : "",
-					cpfCnpj : "",
-					money : 100,
-				}, 
-				clientOrigin : {
-					name : "",
-					email : "",
-					birthDate : "",
-					cpfCnpj : "",
-					money : 10,
-				}
-			}
-		];
-	}
+	}	
 }
