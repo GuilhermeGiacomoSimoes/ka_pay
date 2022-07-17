208c3929506075b6ca230706a1495d02bdf84352
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-17 17:51:26 -0300

    fix: fix names table and variable

--- src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity.ts
@@ -1,12 +1,12 @@
 import {Column, Entity, PrimaryColumn} from "typeorm";
 
-@Entity('transaction')
+@Entity('ka_transaction')
 export class TransactionTypeORMEntity {
 	@PrimaryColumn()
 	id : string;
 
 	@Column({ name : 'value_transaction' })
-	valueTransactio : number;
+	valueTransaction : number;
 
 	@Column({ name : 'client_destination' })
 	clientDestination : string;
