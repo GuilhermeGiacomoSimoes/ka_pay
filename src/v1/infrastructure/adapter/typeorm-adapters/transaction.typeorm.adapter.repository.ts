35fe78a276a2c61c1b1ad456f7af098859845320
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-17 19:40:33 -0300

    feat: create a adapter

--- src/v1/infrastructure/adapter/typeorm-adapters/transaction.typeorm.adapter.repository.ts
+++ src/v1/infrastructure/adapter/typeorm-adapters/transaction.typeorm.adapter.repository.ts
@@ -1,3 +1,4 @@
+import {CLientResponse} from "src/v1/business/common/client.response";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {TransactionTypeORMEntity} from "../../persistence/typeorm/transaction/entity/transaction-typeorm-entity";
 
@@ -5,13 +6,31 @@ export class TransactionTypeormAdapterRepository {
 
 	listTransactionsAdapter(transactionRepositoryTypeorm : TransactionTypeORMEntity[]) : TransactionResponse[] {
 		let transactionsResponse : TransactionResponse[];
-		for(let transactionTypeorm : TransactionTypeORMEntity of transactionRepositoryTypeorm) {
-			const transactionResponse : TransactionResponse;
-			transactionResponse.clientDestination = transactionTypeorm.clientDestination;
-			transactionResponse.clientOrigin = transactionTypeorm.clientOrigin;
-			transactionResponse.valueTransation = transactionTypeorm.valueTransation;
 
+		for(let transactionTypeorm of transactionRepositoryTypeorm) {
+			let transactionResponse : TransactionResponse;
+
+			let clientDestinationResponse : CLientResponse;
+			let clientOriginResponse : CLientResponse;
+
+			clientDestinationResponse.name = transactionTypeorm.clientDestination.name;
+			clientDestinationResponse.email = transactionTypeorm.clientDestination.email;
+			clientDestinationResponse.birthDate = transactionTypeorm.clientDestination.birthDate;
+			clientDestinationResponse.cpfCnpj = transactionTypeorm.clientDestination.cpfCnpj;
+			clientDestinationResponse.money = transactionTypeorm.clientDestination.money;
+
+			clientOriginResponse.name = transactionTypeorm.clientOrigin.name;
+			clientOriginResponse.email = transactionTypeorm.clientOrigin.email;
+			clientOriginResponse.birthDate = transactionTypeorm.clientOrigin.birthDate;
+			clientOriginResponse.cpfCnpj = transactionTypeorm.clientOrigin.cpfCnpj;
+			clientOriginResponse.money = transactionTypeorm.clientOrigin.money;
+
+			transactionResponse.clientDestination = clientDestinationResponse;
+			transactionResponse.clientOrigin = clientOriginResponse;
+			transactionResponse.valueTransaction = transactionTypeorm.valueTransaction;
 			transactionsResponse.push(transactionResponse);	
-		}	
+		}
+
+		return transactionsResponse;
 	}
 }
