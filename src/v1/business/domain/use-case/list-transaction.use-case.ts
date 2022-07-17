ab91083a51ac21d61761b07745915e911f5447c7
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-17 17:51:05 -0300

    feat: the ListTransactionUseCase expect to receive interfface transaction repository

--- src/v1/business/domain/use-case/list-transaction.use-case.ts
+++ src/v1/business/domain/use-case/list-transaction.use-case.ts
@@ -1,8 +1,10 @@
+import {IListTransactionRepository} from "src/v1/infrastructure/persistence/typeorm/transaction/repository/list-transaction.interface.repository";
 import {TransactionResponse} from "../../common/Transaction.response";
 
 export class ListTransactionUseCase {
 
 	constructor(
+		private readonly transactionRepository : IListTransactionRepository 
 	) {}
 
 	execute() : Promise<TransactionResponse> {
