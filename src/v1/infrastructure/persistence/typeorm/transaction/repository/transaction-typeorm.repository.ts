613c5010a378067829e6af42058f23ab6d31e259
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-17 17:52:01 -0300

    chore: implements transaction TypeORMRepository

--- src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
@@ -1,2 +1,18 @@
+import {TransactionResponse} from "src/v1/business/common/Transaction.response";
+import {DataSource, Repository} from "typeorm";
+import {TransactionTypeORMEntity} from "../entity/transaction-typeorm-entity";
+import {ITransactionTypeormRepository} from "./transaction-typeorm.repository.interface";
 
-export default class TransactionTypeORMRepository {}
+export default class TransactionTypeORMRepository implements ITransactionTypeormRepository {
+
+	private readonly ormRepository : Repository<TransactionTypeORMEntity>;
+
+	constructor(private dataSource: DataSource) {
+		this.ormRepository = dataSource.getRepository(TransactionTypeORMEntity);
+	}
+
+	listTransactions(): Promise<TransactionResponse[]> {
+		const transactionsTypeorm : TransactionTypeORMEntity[] = this.ormRepository.find();
+		return  transactions;
+	}
+}
