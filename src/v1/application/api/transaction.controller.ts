f7426795c8e8d3d2a462cbe6d1ee8a552e0456b1
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-19 22:01:35 -0300

    feat: implements CONTROLLER

--- src/v1/application/api/transaction.controller.ts
+++ src/v1/application/api/transaction.controller.ts
@@ -1,13 +1,14 @@
 import {Controller, Get, Inject} from "@nestjs/common";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {ListTransactionUseCase} from "src/v1/business/domain/use-case/list-transaction.use-case";
+import {IListTransactionUseCase} from "src/v1/business/domain/use-case/list-transactions.use-case.interface";
 
 @Controller('transaction')
 export default class TransactionController {
 
 	constructor(
-		@Inject()
-		private readonly listTransationsUseCases : ListTransactionUseCase,	
+		@Inject(ListTransactionUseCase)
+		private readonly listTransationsUseCases : IListTransactionUseCase,	
 	) {}
 
 	@Get()
