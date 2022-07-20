3f95d0828da1b224b85340c9ec01d57e28269c1d
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-19 22:06:29 -0300

    feat: received a datasource

--- src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
@@ -1,14 +1,16 @@
+import {Injectable} from "@nestjs/common";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {TransactionTypeormAdapterRepository} from "src/v1/infrastructure/adapter/typeorm-adapters/transaction.typeorm.adapter.repository";
 import {DataSource, Repository} from "typeorm";
 import {TransactionTypeORMEntity} from "../entity/transaction-typeorm-entity";
 import {ITransactionTypeormRepository} from "./transaction-typeorm.repository.interface";
 
+@Injectable()
 export default class TransactionTypeORMRepository implements ITransactionTypeormRepository {
 
 	private readonly ormRepository : Repository<TransactionTypeORMEntity>;
 
-	constructor(private dataSource: DataSource) {
+	constructor(dataSource: DataSource) {
 		this.ormRepository = dataSource.getRepository(TransactionTypeORMEntity);
 	}
 
