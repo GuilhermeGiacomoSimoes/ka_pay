a551973828f5e09551a138f17bfc745b0b339d73
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-20 23:22:33 -0300

    fix: fix the name

--- src/v1/business/common/Transaction.response.ts
+++ src/v1/business/common/Transaction.response.ts
@@ -1,9 +1,8 @@
-import {Client} from "src/v1/business/domain/entities/client.entity";
 import {CLientResponse} from "./client.response";
 
 export class TransactionResponse {
 	id : string;
 	valueTransaction : number;
 	clientDestination : CLientResponse;
-	clientOrigin : ClientResponse;
+	clientOrigin : CLientResponse;
 }
