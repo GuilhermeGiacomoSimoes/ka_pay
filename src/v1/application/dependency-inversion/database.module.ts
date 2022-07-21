beb4e9803b48d8cf85feff01c8099a27ecf731a9
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-20 23:21:42 -0300

    style: remove uneccessary enter

--- src/v1/application/dependency-inversion/database.module.ts
+++ src/v1/application/dependency-inversion/database.module.ts
@@ -1,7 +1,6 @@
 import {Module} from "@nestjs/common";
 import {databaseProviders} from "src/v1/infrastructure/config/database.config";
 
-
 @Module({
 	providers: [... databaseProviders],
 	exports: [... databaseProviders]
