ec58baf7a1078e0c11aa30193b566e0f9d496a8a
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-23 16:24:24 -0300

    refactor: change localhosto to 127.0.0.1

--- src/v1/infrastructure/config/database.config.ts
+++ src/v1/infrastructure/config/database.config.ts
@@ -6,7 +6,7 @@ export const databaseProviders = [
 		useFactory : async () => {
 			const dataSource = new DataSource({
 				type: 'mysql',
-				host: 'localhost',
+				host: '127.0.0.1',
 				port: 3306,
 				username: 'root',
 				password: 'root',
