92a2b728a4f806bda6cc12418aec9647147621db
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-26 22:29:54 -0300

    fix: fix the module transaction

--- src/v1/application/dependency-inversion/transaction.module.ts
+++ src/v1/application/dependency-inversion/transaction.module.ts
@@ -1,6 +1,8 @@
 import {Module} from "@nestjs/common";
+import {getDataSourceToken} from "@nestjs/typeorm";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {ListTransactionUseCase} from "src/v1/business/use-case/list-transaction.use-case";
+import {TransactionTypeORMEntity} from "src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity";
 import TransactionTypeORMRepository from "src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository";
 import {DataSource} from "typeorm";
 import TransactionController from "../api/transaction.controller";
@@ -9,18 +11,19 @@ import {DatabaseModule} from "./database.module";
 @Module({
 	providers: [
 		{
+			inject: [getDataSourceToken()], 
+			provide: TransactionTypeORMRepository,
+			useFactory(datasource: DataSource)  {
+				const baseRepository = datasource.getRepository<any>(TransactionTypeORMEntity);
+				return new TransactionTypeORMRepository(baseRepository.target, baseRepository.manager, baseRepository.queryRunner);
+			}
+		}, 
+		{
 			provide: ListTransactionUseCase, 
 			useFactory(transactionRepository: ITransactionRepository) {
 				return new ListTransactionUseCase(transactionRepository);
 			},
 			inject: [TransactionTypeORMRepository]
-		},
-		{
-			provide: TransactionTypeORMRepository,
-			useFactory(dataSource: DataSource) {
-				return new TransactionTypeORMRepository(dataSource);
-			}, 
-			inject: []
 		}
 	], 
 	imports: [DatabaseModule],
