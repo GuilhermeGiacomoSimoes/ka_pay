79d8c81aacac19856a357134f32d95b89f9b27bf
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-26 22:30:53 -0300

    refactor: the constructor is in Repository parent class

--- src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
@@ -1,21 +1,17 @@
 import {Injectable} from "@nestjs/common";
+import {CustomRepository} from "src/v1/application/dependency-inversion/typeorm.decorator";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {TransactionTypeormAdapterRepository} from "src/v1/infrastructure/adapter/typeorm-adapters/transaction.typeorm.adapter.repository";
-import {DataSource, Repository} from "typeorm";
+import { Repository} from "typeorm";
 import {TransactionTypeORMEntity} from "../entity/transaction-typeorm-entity";
 
+@CustomRepository(TransactionTypeORMEntity)
 @Injectable()
-export default class TransactionTypeORMRepository implements ITransactionRepository {
-
-	private readonly ormRepository : Repository<TransactionTypeORMEntity>;
-
-	constructor(dataSource: DataSource) {
-		this.ormRepository = dataSource.getRepository(TransactionTypeORMEntity);
-	}
+export default class TransactionTypeORMRepository extends Repository<TransactionTypeORMEntity> implements ITransactionRepository {
 
 	async listTransactions(): Promise<TransactionResponse[]> {
-		const transactionsTypeorm : TransactionTypeORMEntity[] = await this.ormRepository.find();
+		const transactionsTypeorm : TransactionTypeORMEntity[] = await this.find();
 		const transactionAdapter = new TransactionTypeormAdapterRepository();
 		let transactions = transactionAdapter.listTransactionsAdapter(transactionsTypeorm);
 		return  transactions;
