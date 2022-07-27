74d35409fa57f91b7408b36f723310f9c47c3cf7
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-26 22:30:28 -0300

    feat: inject the repository in use-case

--- src/v1/business/use-case/list-transaction.use-case.ts
+++ src/v1/business/use-case/list-transaction.use-case.ts
@@ -1,12 +1,15 @@
 import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
 import {TransactionResponse} from "../common/Transaction.response";
 import {ITransactionRepository} from "../domain/transaction-typeorm.repository.interface";
 import {IListTransactionUseCase} from "./list-transactions.use-case.interface";
+import TransactionTypeORMRepository from 'src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository'; 
 
 @Injectable()
 export class ListTransactionUseCase implements IListTransactionUseCase {
 
 	constructor(
+		@InjectRepository(TransactionTypeORMRepository)
 		private readonly transactionRepository : ITransactionRepository 
 	) {}
 
