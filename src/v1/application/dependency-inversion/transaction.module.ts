4bab611ea005a837049f353531a80fb653bcc5ca
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-07-28 22:32:37 -0300

    refactor: change to before

--- src/v1/application/dependency-inversion/transaction.module.ts
+++ src/v1/application/dependency-inversion/transaction.module.ts
@@ -2,7 +2,6 @@ import {Module} from "@nestjs/common";
 import {getDataSourceToken} from "@nestjs/typeorm";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {ListTransactionUseCase} from "src/v1/business/use-case/list-transaction.use-case";
-import {TransactionTypeORMEntity} from "src/v1/infrastructure/persistence/typeorm/transaction/entity/transaction-typeorm-entity";
 import TransactionTypeORMRepository from "src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository";
 import {DataSource} from "typeorm";
 import TransactionController from "../api/transaction.controller";
@@ -13,9 +12,8 @@ import {DatabaseModule} from "./database.module";
 		{
 			inject: [getDataSourceToken()], 
 			provide: TransactionTypeORMRepository,
-			useFactory(datasource: DataSource)  {
-				const baseRepository = datasource.getRepository<any>(TransactionTypeORMEntity);
-				return new TransactionTypeORMRepository(baseRepository.target, baseRepository.manager, baseRepository.queryRunner);
+			useFactory(dataSource: DataSource) {
+				return new TransactionTypeORMRepository(dataSource);
 			}
 		}, 
 		{
