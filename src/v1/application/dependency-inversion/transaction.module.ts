4254620e8d52d28997176da65fc569c1dcf5c96a
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-01 22:00:33 -0300

    provide and inject token

--- src/v1/application/dependency-inversion/transaction.module.ts
+++ src/v1/application/dependency-inversion/transaction.module.ts
@@ -1,27 +1,27 @@
 import {Module} from "@nestjs/common";
-import {getDataSourceToken} from "@nestjs/typeorm";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {ListTransactionUseCase} from "src/v1/business/use-case/list-transaction.use-case";
 import TransactionTypeORMRepository from "src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository";
 import {DataSource} from "typeorm";
 import TransactionController from "../api/transaction.controller";
 import {DatabaseModule} from "./database.module";
+import {MySqlDataDourceToken, TransactionRepositoryToken} from "./typeorm.decorator";
 
 @Module({
 	providers: [
 		{
-			inject: [getDataSourceToken()], 
-			provide: TransactionTypeORMRepository,
+			provide: TransactionRepositoryToken,
 			useFactory(dataSource: DataSource) {
 				return new TransactionTypeORMRepository(dataSource);
-			}
+			}, 
+			inject: [MySqlDataDourceToken], 
 		}, 
 		{
 			provide: ListTransactionUseCase, 
 			useFactory(transactionRepository: ITransactionRepository) {
 				return new ListTransactionUseCase(transactionRepository);
 			},
-			inject: [TransactionTypeORMRepository]
+			inject: [TransactionRepositoryToken]
 		}
 	], 
 	imports: [DatabaseModule],
