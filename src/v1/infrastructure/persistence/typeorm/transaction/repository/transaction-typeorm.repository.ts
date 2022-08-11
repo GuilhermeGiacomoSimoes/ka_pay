b4a04642d0d01a39b0d1cc5a9918f37a1cf92fe8
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-10 21:15:29 -0300

    received dataSource in constructor

--- src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
+++ src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository.ts
@@ -2,14 +2,19 @@ import {Injectable} from "@nestjs/common";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {TransactionTypeormAdapterRepository} from "src/v1/infrastructure/adapter/typeorm-adapters/transaction.typeorm.adapter.repository";
-import { Repository} from "typeorm";
+import {DataSource, Repository} from "typeorm";
 import {TransactionTypeORMEntity} from "../entity/transaction-typeorm-entity";
 
 @Injectable()
-export default class TransactionTypeORMRepository extends Repository<TransactionTypeORMEntity> implements ITransactionRepository {
+export default class TransactionTypeORMRepository implements ITransactionRepository {
+
+	private readonly dataSource: Repository<TransactionTypeORMEntity>;
+	constructor(dataSource: DataSource) {
+		this.dataSource = dataSource.getRepository(TransactionTypeORMEntity) 
+	}
 
 	async listTransactions(): Promise<TransactionResponse[]> {
-		const transactionsTypeorm : TransactionTypeORMEntity[] = await this.find();
+		const transactionsTypeorm : TransactionTypeORMEntity[] = await this.dataSource.find();
 		const transactionAdapter = new TransactionTypeormAdapterRepository();
 		let transactions = transactionAdapter.listTransactionsAdapter(transactionsTypeorm);
 		return  transactions;
