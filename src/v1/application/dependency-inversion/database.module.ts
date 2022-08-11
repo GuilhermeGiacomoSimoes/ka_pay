a5548c5edb5ee988eeed6953a027aa00198f7e5f
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-10 21:14:17 -0300

    increment a database module

--- src/v1/application/dependency-inversion/database.module.ts
+++ src/v1/application/dependency-inversion/database.module.ts
@@ -1,8 +1,22 @@
-import {Module} from "@nestjs/common";
+import {Inject, Module} from "@nestjs/common";
 import {databaseProviders} from "src/v1/infrastructure/config/database.config";
+import {DataSource} from "typeorm";
+import { MySqlDataDourceToken} from "./typeorm.decorator";
 
 @Module({
 	providers: [... databaseProviders],
 	exports: [... databaseProviders]
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+	constructor(
+		@Inject(MySqlDataDourceToken) private dataSource: DataSource
+	) {}
+
+	async onModuleInit() {
+		await this.dataSource?.initialize();
+	}
+
+	async onModuleDestroy() {
+		await this.dataSource?.destroy();
+	}
+}
