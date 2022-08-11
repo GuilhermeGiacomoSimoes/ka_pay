9f82b1aba25d26f8d2b649e628f232adb8af0c9a
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-10 21:14:35 -0300

    refactor the transaction module

--- src/v1/application/dependency-inversion/transaction.module.ts
+++ src/v1/application/dependency-inversion/transaction.module.ts
@@ -1,71 +1,30 @@
-import {DynamicModule, Provider} from "@nestjs/common";
-import {getDataSourceToken} from "@nestjs/typeorm";
+import {Module} from "@nestjs/common";
 import {ITransactionRepository} from "src/v1/business/domain/transaction-typeorm.repository.interface";
 import {ListTransactionUseCase} from "src/v1/business/use-case/list-transaction.use-case";
 import TransactionTypeORMRepository from "src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository";
 import {DataSource} from "typeorm";
 import TransactionController from "../api/transaction.controller";
 import {DatabaseModule} from "./database.module";
-import {TYPEORM_CUSTOM_REPOSITORY} from "./typeorm.decorator";
+import {ListTransactionUseCaseToken, MySqlDataDourceToken, TransactionRepositoryToken} from "./typeorm.decorator";
 
 @Module({
 	providers: [
 		{
-			provide: TransactionRepositoryToken,
-			useFactory(dataSource: DataSource) {
-				return new TransactionTypeORMRepository(dataSource);
-			}, 
-			inject: [MySqlDataDourceToken], 
-		}, 
-		{
-			provide: ListTransactionUseCase, 
+			provide: ListTransactionUseCaseToken,
 			useFactory(transactionRepository: ITransactionRepository) {
-				return new ListTransactionUseCase(transactionRepository);
+				return new ListTransactionUseCase(transactionRepository)	
 			},
 			inject: [TransactionRepositoryToken]
-=======
-import {TYPEORM_CUSTOM_REPOSITORY} from "./typeorm.decorator";
-
-export class TransactionModule {
-	public static forCustomRepository<T extends new (...args: any[]) => any>(repositories: T[]): DynamicModule {
-
-		const providers: Provider[] = [];
-
-		for(const repository of repositories) {
-			const entity = Reflect.getMetadata(TYPEORM_CUSTOM_REPOSITORY, repository)
-			if(!entity) {
-				continue;
-			}
-
-			providers.push({
-				inject: [getDataSourceToken()], 
-				provide: repository,
-				useFactory: (dataSource: DataSource) => {
-					const baseRepository = dataSource.getRepository<any>(entity);
-					return new repository(baseRepository.target, baseRepository.manager, baseRepository.queryRunner);
-				}
-			})	
+		}, 
+		{
+			provide: TransactionRepositoryToken,
+			useFactory(dataSource: DataSource) {
+				return new TransactionTypeORMRepository(dataSource)	
+			},
+			inject: [MySqlDataDourceToken]
 		}
-
-		return {
-			exports: [...providers, {
-				provide: ListTransactionUseCase, 
-				useFactory(transactionRepository: ITransactionRepository) {
-					return new ListTransactionUseCase(transactionRepository);
-				},
-				inject: [TransactionTypeORMRepository]
-			}],
-			imports: [DatabaseModule],
-			controllers: [TransactionController], 
-			module: TransactionModule,
-			providers: [...providers, {
-				provide: ListTransactionUseCase, 
-				useFactory(transactionRepository: ITransactionRepository) {
-					return new ListTransactionUseCase(transactionRepository);
-				},
-				inject: [TransactionTypeORMRepository]
-			}],
->>>>>>> abeff624977a8c59b51e2a065b4b6e9f6b7b096b
-		}
-	}
-}
+	],
+	imports: [DatabaseModule],
+	controllers: [TransactionController]
+})
+export class TransactionModule {}
