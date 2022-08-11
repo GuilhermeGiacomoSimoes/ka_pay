88e99cbb322773cc47eead2679792b0402fbfde8
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-10 21:15:15 -0300

    import the transaction module to modules

--- src/v1/application/dependency-inversion/v1.module.ts
+++ src/v1/application/dependency-inversion/v1.module.ts
@@ -1,11 +1,10 @@
 import {Module} from "@nestjs/common";
 import {RouterModule} from "@nestjs/core";
-import TransactionTypeORMRepository from "src/v1/infrastructure/persistence/typeorm/transaction/repository/transaction-typeorm.repository";
 import {DatabaseModule} from "./database.module";
 import {TransactionModule} from "./transaction.module";
 
 const modules = [
-	TransactionModule.forCustomRepository([TransactionTypeORMRepository])
+	TransactionModule
 ];
 
 @Module({
