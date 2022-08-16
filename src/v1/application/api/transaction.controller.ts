793e7770d1838c7d23ec2b3087eedb4378b1f376
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-15 21:02:08 -0300

    fix: the inject must be a token

--- src/v1/application/api/transaction.controller.ts
+++ src/v1/application/api/transaction.controller.ts
@@ -1,13 +1,13 @@
 import {Controller, Get, Inject} from "@nestjs/common";
 import {TransactionResponse} from "src/v1/business/common/Transaction.response";
 import {IListTransactionUseCase} from "src/v1/business/use-case/list-transactions.use-case.interface";
-import {ListTransactionUseCase} from 'src/v1/business/use-case/list-transaction.use-case';
+import { ListTransactionUseCaseToken } from "../dependency-inversion/typeorm.decorator";
 
 @Controller('transaction')
 export default class TransactionController {
 
 	constructor(
-		@Inject(ListTransactionUseCase)
+		@Inject(ListTransactionUseCaseToken)
 		private readonly listTransationsUseCases : IListTransactionUseCase,	
 	) {}
 
