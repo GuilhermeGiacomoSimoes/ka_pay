d33a55122b543942eb5f7fbd96b01b38d3092986
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-08-28 19:50:18 -0300

    fix the returns

--- src/domain/interfaceAdapters/repository/TransactionRepositoryInterface.ts
+++ src/domain/interfaceAdapters/repository/TransactionRepositoryInterface.ts
@@ -1,8 +1,8 @@
-import {Transaction} from "../entities/Transaction";
+import {Transaction} from "../../entities/Transaction";
 
 export interface TransactionRepositoryInterface {
 	listTransactions(): Promise<Transaction[]>;
-	getTransactionById(uuid: string): Promise<Transaction>;
-	save(transaction: Transaction): Transaction;
-	update(transaction: Transaction): Transaction;
+	getTransactionById(uuid: string): Promise<Transaction | undefined>;
+	save(transaction: Transaction): any;
+	update(transaction: Transaction): any;
 }
