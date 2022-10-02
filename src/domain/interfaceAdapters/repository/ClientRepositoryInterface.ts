7e5c044cdf98e5ab6b6cae5968402a8219b68153
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-10-02 16:56:08 -0300

    feat: the functions must be a undefined

--- src/domain/interfaceAdapters/repository/ClientRepositoryInterface.ts
+++ src/domain/interfaceAdapters/repository/ClientRepositoryInterface.ts
@@ -2,7 +2,7 @@ import {Client} from "../../entities/Client";
 
 export interface ClientRepositoryInterface {
 	save(client: Client) : Promise<Client>;
-	update(client: Client) : Promise<Client>;
-	getClientById(uuid: string) : Promise<Client>;
+	update(client: Client) : Promise<Client | undefined>;
+	getClientById(uuid: string) : Promise<Client | undefined>;
 	getAll(): Promise<Client[]>;
 }
