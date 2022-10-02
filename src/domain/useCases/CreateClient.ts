78e54ce522bdb0cd0fe299ce71c08e344b05443c
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-10-02 16:57:50 -0300

    feat: create use cases to create client

---
+++ src/domain/useCases/CreateClient.ts
@@ -0,0 +1,17 @@
+import {Client} from "../entities/Client";
+import {ClientRepositoryInterface} from "../interfaceAdapters/repository/ClientRepositoryInterface";
+
+export class CreateClient {
+	private readonly clientRepository: ClientRepositoryInterface;
+	constructor(clientRepository: ClientRepositoryInterface) {
+		this.clientRepository = clientRepository;
+	}
+
+	async execute(client: Client) {
+		const clientConsult = this.clientRepository.getClientById(client.id);
+		if(clientConsult == undefined) {
+			throw new Error("Duplicate key");
+		}
+		this.clientRepository.save(client);
+	}
+}
