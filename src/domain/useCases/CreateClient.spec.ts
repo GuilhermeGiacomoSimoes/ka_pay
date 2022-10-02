aff7404f3998b00b1548391bff2600576941085d
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-10-02 16:58:08 -0300

    test: create the test to use cases create client

---
+++ src/domain/useCases/CreateClient.spec.ts
@@ -0,0 +1,26 @@
+import {CreateClient} from "./CreateClient";
+import {ClientRepositoryInterface} from "../interfaceAdapters/repository/ClientRepositoryInterface";
+import {ClientRepositoryMemory} from "../interfaceAdapters/repository/inMemory/ClientRepositoryMemory";
+import {Client} from "../entities/Client";
+
+describe('Create Client Use Cases', () => {
+	
+	let repositoryClient : ClientRepositoryInterface; 
+	let createCliente : CreateClient;
+
+	beforeEach(() => {
+		repositoryClient = new ClientRepositoryMemory();
+		createCliente = new CreateClient(repositoryClient);
+	});
+
+	test('verify if save correct and not throw', async () => {
+		const client = new Client('id', 'guilherme', '15/01/1996', '1111111111');
+		expect(createCliente.execute(client)).resolves.not.toThrow();
+	});
+
+	test('verify if return throw', async () => {
+		const client = new Client('id', 'guilherme', '15/01/1996', '1111111111');
+		createCliente.execute(client);
+		expect(createCliente.execute(client)).rejects.toThrow();
+	});
+});
