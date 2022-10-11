6f8898af782a453ba87f77e5cc270bfc8c8449f2
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-10-10 22:26:31 -0300

    refactor: add await in call functions

--- src/domain/useCases/CreateClient.spec.ts
+++ src/domain/useCases/CreateClient.spec.ts
@@ -13,12 +13,12 @@ describe('Create Client Use Cases', () => {
 
 	test('verify if save correct and not throw', async () => {
 		const client = new Client('id', 'guilherme', '15/01/1996', '1111111111');
-		expect(createCliente.execute(client)).resolves;
+		expect(await createCliente.execute(client)).resolves.not.toThrow();
 	});
 
 	test('verify if return throw', async () => {
 		const client = new Client('id', 'guilherme', '15/01/1996', '1111111111');
-		createCliente.execute(client);
-		expect(createCliente.execute(client)).rejects;
+		await createCliente.execute(client);
+		expect(await createCliente.execute(client)).rejects.toThrow();
 	});
 });
