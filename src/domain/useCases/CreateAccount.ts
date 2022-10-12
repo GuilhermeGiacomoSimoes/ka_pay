418dbd5f411b3b9c926370a1fdd9f3dbdb4a5a19
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-10-12 13:05:29 -0300

    feat: create in useCase verify if client ou bank nonexists

--- src/domain/useCases/CreateAccount.ts
+++ src/domain/useCases/CreateAccount.ts
@@ -1,15 +1,30 @@
 import {Account} from "../entities/Account";
 import {AccountRepositoryInterface} from "../interfaceAdapters/repository/AccountRepositoryInterface";
+import {BankRepositoryInterface} from "../interfaceAdapters/repository/BankRepositoryInterface";
+import {ClientRepositoryInterface} from "../interfaceAdapters/repository/ClientRepositoryInterface";
 
 export class CreateAccount {
-	private readonly repository: AccountRepositoryInterface;
-	constructor(repository: AccountRepositoryInterface) {
-		this.repository = repository;
+	private readonly repositoryAccount: AccountRepositoryInterface;
+	private readonly repositoryClient : ClientRepositoryInterface;
+	private readonly repositoryBank : BankRepositoryInterface;
+
+	constructor(repositoryAccount: AccountRepositoryInterface, repositoryClient: ClientRepositoryInterface, repositoryBank: BankRepositoryInterface) {
+		this.repositoryAccount = repositoryAccount;
+		this.repositoryClient = repositoryClient;
+		this.repositoryBank = repositoryBank;
 	}
 
 	async execute(account: Account) {
-		const accountConsult = await this.repository.getAccountById(account.id);
+		const accountConsult = await this.repositoryAccount.getAccountById(account.id);
 		if(accountConsult) throw new Error('account consult already exist');
-		this.repository.save(account);
+
+		const clientConsult = await this.repositoryClient.getClientById(account.idClient);
+		const bankConsult = await this.repositoryBank.getBankById(account.idBank);
+
+		if(!clientConsult || !bankConsult) {
+			throw new Error('Client or Bank is not exists');
+		}
+
+		this.repositoryAccount.save(account);
 	}
 }
