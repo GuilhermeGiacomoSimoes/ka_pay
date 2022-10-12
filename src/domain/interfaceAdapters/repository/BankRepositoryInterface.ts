143a2fab95bc2067b907c1c1df1f90f8769056e9
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-10-12 11:05:15 -0300

    refactor: add | undefined in returns

--- src/domain/interfaceAdapters/repository/BankRepositoryInterface.ts
+++ src/domain/interfaceAdapters/repository/BankRepositoryInterface.ts
@@ -1,8 +1,8 @@
 import {Bank} from "../../entities/Bank";
 
 export interface BankRepositoryInterface {
-	save(bank: Bank) : Promise<Bank>;
-	update(bank: Bank) : Promise<Bank>;
-	getBankById(uuid: string) : Promise<Bank>;
+	save(bank: Bank) : Promise<Bank | undefined>;
+	update(bank: Bank) : Promise<Bank | undefined>;
+	getBankById(uuid: string) : Promise<Bank | undefined>;
 	getAll(): Promise<Bank[]>;
 }
