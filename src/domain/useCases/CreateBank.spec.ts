cccd2f7423cabb03cc3c024fe35db722e44e460b
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-10-12 11:06:22 -0300

    test: create test to useCase createBank

---
+++ src/domain/useCases/CreateBank.spec.ts
@@ -0,0 +1,23 @@
+import {Bank} from "../entities/Bank";
+import {BankRepositoryMemory} from "../interfaceAdapters/repository/inMemory/BankRepositoryMemory";
+import {CreateBank} from "./CreateBank";
+
+describe('useCase - createBank', () => {
+
+	let createBank : CreateBank;
+	beforeEach(() => {
+		const repository = new BankRepositoryMemory();
+		createBank = new CreateBank(repository);
+	});
+
+	test('create bank not throw', async () => {
+		const client = new Bank('id', 'guilherme'); 
+		expect(createBank.execute(client)).resolves.not.toThrow();
+	});
+
+	test('create duplicate bank throw', async () => {
+		const bank = new Bank('id', 'guilherme'); 
+		await createBank.execute(bank);
+		expect(createBank.execute(bank)).rejects.toThrow();
+	});
+});
