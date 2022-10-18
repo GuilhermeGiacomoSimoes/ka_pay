161853bf1988eebcc405eeeddac3d6ec21c174e7
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-10-17 22:16:29 -0300

    feat: add function app.put to clien tin index.ts

--- src/driversAndFrameworks/index.ts
+++ src/driversAndFrameworks/index.ts
@@ -1,5 +1,8 @@
 import express, { Express , Request, Response} from 'express';
 import dotenv from 'dotenv';
+import {ClientRepositoryMySQL} from './ClientRepositoryMySQL';
+import {CreateClient} from '../domain/useCases/CreateClient';
+import {Client} from '../domain/entities/Client';
 
 dotenv.config();
 
@@ -9,6 +12,13 @@ const port = 8080;
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Express + TypeScript server');
+});
+
+app.put('/client', (req: Request, res: Response) => {
+	const repositoryClient = new ClientRepositoryMySQL();
+	const createClientUseCase = new CreateClient(repositoryClient);
+	const client: Client = new Client('id', 'guilherme', '15/01/1996', '111222222333');
+	createClientUseCase.execute(client);
 });
 
 app.listen(port, () => {
