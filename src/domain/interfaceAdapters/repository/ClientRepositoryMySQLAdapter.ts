ec3475588ef835d43a9e4999c081c52d6efd4bc8
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-12-13 22:06:20 -0300

    refactor: undefined to null

--- src/domain/interfaceAdapters/repository/ClientRepositoryMySQLAdapter.ts
+++ src/domain/interfaceAdapters/repository/ClientRepositoryMySQLAdapter.ts
@@ -2,7 +2,8 @@ import {ClientInterfaceMySQLDTO} from "../../../driversAndFrameworks/ClientMySQL
 import {Client} from "../../entities/Client";
 
 export abstract class ClientRepositoryMySQLAdapter {
-	static execute(cleintDTO : ClientInterfaceMySQLDTO) : Client {
+	static execute(cleintDTO : ClientInterfaceMySQLDTO | undefined) : Client | null {
+		if(!cleintDTO) return null;
 		const client = new Client(cleintDTO.id,cleintDTO.name,cleintDTO.birth_date, cleintDTO.cpf_cnpj);	
 		return client;
 	}
--- src/driversAndFrameworks/ClientRepositoryMySQL.ts
+++ src/driversAndFrameworks/ClientRepositoryMySQL.ts
@@ -6,13 +6,13 @@ import {execute} from "./mysqlconfig";
 
 export class ClientRepositoryMySQL implements ClientRepositoryInterface {
 
-	async save(client: Client) : Promise<Client> {
+	async save(client: Client) : Promise<Client | null> {
 		const query = `INSERT INTO ka_clients(name,birth_date,cpf_cnpj) VALUES (?,?,?);`;
 		const clientDTO = await execute<ClientInterfaceMySQLDTO>(query,[client.name, client.birthDate, client.cpfCnpj]);
 		return Promise.resolve(ClientRepositoryMySQLAdapter.execute(clientDTO));
 	}
 	
-	async update(client: Client) : Promise<Client | undefined> {
+	async update(client: Client) : Promise<Client | null> {
 		const query = `
 		UPDATE FROM ka_clients 
 		SET 	
@@ -33,7 +33,7 @@ export class ClientRepositoryMySQL implements ClientRepositoryInterface {
 		return Promise.resolve(ClientRepositoryMySQLAdapter.execute(clientDTO));
 	}
 	
-	async getClientById(uuid: string) : Promise<Client | undefined> {
+	async getClientById(uuid: string) : Promise<Client | null> {
 		const query = `
 		SELECT 
 			* 
