93abd8a363bd5d435e340160f96092db3d7e5184
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-12-13 22:19:54 -0300

    refactor: undefined to null

--- src/domain/interfaceAdapters/repository/inMemory/ClientRepositoryMemory.ts
+++ src/domain/interfaceAdapters/repository/inMemory/ClientRepositoryMemory.ts
@@ -9,12 +9,12 @@ export class ClientRepositoryMemory implements ClientRepositoryInterface{
 		return Promise.resolve(this.clients);	
 	}	
 
-	getClientById(uuid: string): Promise<Client | undefined> {
+	getClientById(uuid: string): Promise<Client | null> {
 		const client : Client | undefined = 
 			this.clients.find((transactiom) => {
 				    return transactiom.id == uuid;
 			}); 
-		return Promise.resolve(client);
+		return Promise.resolve(client || null);
 	}
 
 	save(client: Client): Promise<Client> {
@@ -22,10 +22,10 @@ export class ClientRepositoryMemory implements ClientRepositoryInterface{
 		return Promise.resolve(client);
 	}
 
-	update(client: Client) : Promise<Client | undefined> {
+	update(client: Client) : Promise<Client | null> {
 		const indexClientUpdate = this.returnIndexOfClientInArrayMemoryByUuid(client.id);
 		if(indexClientUpdate == -1) {
-			return Promise.resolve(undefined);
+			return Promise.resolve(null);
 		}
 
 		this.clients.splice(indexClientUpdate, 1);
