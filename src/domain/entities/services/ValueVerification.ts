c19949b5b9777316857bc1990cebbbfcd189ac3b
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2023-03-29 23:15:34 -0300

    chore: add type

--- src/domain/entities/services/ValueVerification.ts
+++ src/domain/entities/services/ValueVerification.ts
@@ -1,6 +1,6 @@
 import { err } from "./error";
 
-export function valueVerification(value: number) {
+export function valueVerification(value: number) : void {
 	if(value < 0) err("Value is not valid");
     if(value == null || value == undefined || isNaN(value)) err("Value is not a number");
 }
