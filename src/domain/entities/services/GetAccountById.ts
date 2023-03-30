e6361e4735993dfe4a8725ff6c87b5c0d7bcd364
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2023-03-30 19:39:11 -0300

    feat: function should return Promise<Account>

--- src/domain/entities/services/GetAccountById.ts
+++ src/domain/entities/services/GetAccountById.ts
@@ -1,7 +1,8 @@
 import { AccountRepositoryInterface } from "../../interfaceAdapters/interfaces/repository/AccountRepositoryInterface";
+import { Account } from "../Account";
 
-export async function getAccountById(uuid: string, repository: AccountRepositoryInterface) {
-    const account = repository.getAccountById(uuid);
+export async function getAccountById(uuid: string, repository: AccountRepositoryInterface): Promise<Account> {
+    const account = await repository.getAccountById(uuid);
 	if(account == undefined || account == null) throw new Error('Account not found');
     return account;
 }
