5274644de302137f6ea393ff5dc902755873cb73
guilherme giacomo simoes <trintaeoitogc@gmail.com>
2023-05-20 01:00:07 -0300

    refactor: revert order

--- src/domain/entities/services/ValueVerification.ts
+++ src/domain/entities/services/ValueVerification.ts
@@ -1,6 +1,6 @@
 import { err } from "./error";
 
-export function valueVerification(value: number) : void {
-	if(value < 0) err("Value is not valid");
+export async function valueVerification(value: number) : Promise<void> {
     if(value == null || value == undefined || isNaN(value)) err("Value is not a number");
+	if(value < 0) err("Value is not valid");
 }
